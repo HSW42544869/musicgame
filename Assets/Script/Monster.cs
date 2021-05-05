cc3f112694a5bdabf8d2c815465b3b1ab7f2b838
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-05 21:30:03 +0800

    5/5 21.

---
+++ Assets/Script/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [Header("跳躍高度"),Range(0,3000)]
+    public int jump = 100;
+    [Header("血量"),Range(0,200)]
+    public int live = 500;
+    public bool infloot = false;
+
+    private int score = 0;
+    private AudioClip aud1;
+    private AudioClip aud2;
+    private AudioSource As;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+}
