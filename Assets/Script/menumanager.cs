64dd9627d94c68830000ffc4536a20ff23bed38f
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-12 19:58:15 +0800

    5/12 19.

---
+++ Assets/Script/menumanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menumanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        Invoke("DelayStart", 2.5F);
+    }
+    private void DelayStart()
+    {
+        SceneManager.LoadScene("關卡");
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Invoke("DelayQuit", 2.5F);
+    }
+    private void DelayQuit()
+    {
+        Application.Quit();
+    }
+}
